ec488c63d3a65b70922db8d7bb769f287f63ec68
ayomiposy <georgejoshuaayomiposi@gmail.com>
2025-06-15 21:30:28 +0100

    Added bracket sign, backspace function and history function

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ This currnt version can only perform simple expression
 the version can carryout basic exponential
 Feature to calculate display screen 
 framework... flexbox and grid was used when styling the css
+
+
+#   15/ 06 / 2025:
+
+Added the bracket sign, backspace function and function to generate history
--- index.js
+++ index.js
@@ -2,16 +2,27 @@
 // Get value from input button to display screen
 const display = document.getElementById("display");
 
+// Create an empty history array to store calculations
+let showHistory = [];
+
 
 // Added function to append value to the calculator display
-function appendToDisplay(input) {
-    display.value += input;
+function appendToDisplay(textarea) {
+    display.value += textarea;
 }
 
 
 // Function to clear screen. this replace the value on screen with empty string
 function clearScreen() {
     display.value = ""
+    display.style.fontSize = "50px"; // Reset font size
+    display.style.overflowY = "hidden"; // Reset overflow
+}
+
+// Function to backspace. 
+
+function backSpace() {
+    display.value = display.value.slice(0, -1);
 }
 
 
@@ -36,8 +47,20 @@ function calculate() {
 
     try {
         display.value = eval(expression)
+        // store the result in the array if the calculation is successful and make sure the result is taking the firt position of the array
+        showHistory.unshift(eval(expression));
     }
     catch {
         display.value = "Error"
     }
+}
+
+
+// Function to display history of calculations
+function history() {
+    showHistory = showHistory.slice(0, 5); // Limit to last 5 calculations
+    display.value = ("History:\n" + showHistory.join("\n")); // Join the history array into a string with line breaks
+    display.style.overflowY = "scroll";
+    display.style.height = "100px";
+    display.style.fontSize = "14px";
 }
